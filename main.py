4c135d4e09970920fc6be0cccf49bd51fc737c20
Karennarodriguez <kr2266@nyu.edu>
2021-11-20 19:15:04 -0500

    search

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#get to the correct directory
 #source venv/bin/activate
 #export FLASK_APP=main.py
 #flask run
@@ -23,29 +24,43 @@ conn = pymysql.connect(host = '127.0.0.1',
 					   cursorclass = pymysql.cursors.DictCursor)
 	
 #PUBLIC VIEW
-@app.route('/', methods = ["GET", "POST"])
+@app.route('/')
+def public_search(): 
+    return render_template("public_search.html")
+
+@app.route('/public_view', methods = ["GET", "POST"])
 def public_view(): 
     #today = str(date.today())
     #print(today)
-    query = 'SELECT * FROM flight WHERE departure_date >= "11-19-2021"'
+
+    query = "SELECT * FROM flight WHERE departure_date >= '11-20-2021'"
     
     queries = []
     departure_date = request.form.get('departure_date')
-    print(departure_date)
     arrival_date = request.form.get('arrival_date')
-    print(arrival_date)
-    departure_city = request.form.get('departure_city')
-    arrival_city = request.form.get('arrival_city')
+    #departure_city = request.form.get('departure_city')
+    #arrival_city = request.form.get('arrival_city')
     departure_airport = request.form.get('departure_airport')
-    print(departure_airport)
     arrival_airport = request.form.get('arrival_airport')
-    print(arrival_airport)
+
+    if departure_airport != '':
+        queries.append("departure_airport = '%s'" % departure_airport)
+    if arrival_airport != '':
+        queries.append("arrival_airport = '%s'" % arrival_airport)
+    if departure_date != '':
+        queries.append("departure_date = '%s'" % departure_date)
+    if arrival_date != '':
+        queries.append("arrival_date = '%s'" % arrival_date)
+    if queries:
+        query += " AND " + " AND ".join(queries)
+    print(query) 
+
 
     #execute queries from database
     cursor = conn.cursor()
     cursor.execute(query)
     data = cursor.fetchall()
-    print(data)
+    
     cursor.close()
     return render_template('public_view.html', data=data)
 
